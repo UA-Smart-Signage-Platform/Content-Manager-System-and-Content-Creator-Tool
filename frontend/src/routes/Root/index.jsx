422d998cb518937a6911f68c4f5ba62e6083fa5d
Tomas Victal <tomasvictal@gmail.com>
2024-03-18 23:37:31 +0000

    Started Folder structure, Remove some unused folder, tailwindWorkin, basic ReactRouterSetup with Root

---
+++ frontend/src/routes/Root/index.jsx
@@ -0,0 +1,13 @@
+
+
+function Root(){
+
+    return(
+        <div className=" flex h-[100vh]">
+            <div className=" w-[10%] bg-black h-full">dw</div>
+            <div className=" w-[90%] bg-slate-500 h-full"></div>
+        </div>
+    )
+}
+
+export default Root;
