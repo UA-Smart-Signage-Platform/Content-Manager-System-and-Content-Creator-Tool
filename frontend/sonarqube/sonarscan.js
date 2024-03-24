d73b36509c295cd98378644aae4327a452ba1f82
pdPinho <pd.pinho@ua.pt>
2024-03-24 12:01:10 +0000

    added frontend src scan folder and updated gitignore

--- frontend/sonarqube/sonarscan.js
+++ frontend/sonarqube/sonarscan.js
@@ -3,13 +3,14 @@ const scanner = require('sonarqube-scanner');
 scanner(
     {
         serverUrl: 'http://sonarqube:9000',
-        token: "sqp_4a8ac99ccd3dee8c55845ab16577317445c48b46",
+        token: "sqp_b9e9608eca733ff669dd65a210508969dba53feb",
         options: {
-            'sonar.projectName': 'PI-frontend',
+            'sonar.projectName': 'react-no-restart',
             'sonar.projectDescription': 'Here I can add a description of my project',
-            'sonar.projectKey': 'PI-frontend',
+            'sonar.projectKey': 'react-no-restart',
             'sonar.projectVersion': '0.0.1',
             'sonar.exclusions': '',
+            'sonar.sources': 'src',
             'sonar.sourceEncoding': 'UTF-8',
         }
     },
