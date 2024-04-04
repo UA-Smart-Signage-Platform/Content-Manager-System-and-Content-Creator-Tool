ec573dd3479f8ac818df79f5fcf17eb7d9213a51
miguel <miguelcruzeiro70@gmail.com>
2024-04-04 15:12:26 +0100

    docker compose for postgres

---
+++ backend/uasmartsignage/src/main/java/deti/uas/uasmartsignage/UasmartsignageApplication.java
@@ -0,0 +1,18 @@
+package deti.uas.uasmartsignage;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class UasmartsignageApplication {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(UasmartsignageApplication.class, args);
+    }
+
+
+}
