48a1fd0bcd4b2e729de1bb8d88902db816fa29f7
DiogoSilva1904 <diogompsilva2000@gmail.com>
2024-04-13 17:46:48 +0100

    workflow update

--- backend/uasmartsignage/src/main/java/deti/uas/uasmartsignage/Test/J.java
+++ backend/uasmartsignage/src/main/java/deti/uas/uasmartsignage/Test/J.java
@@ -3,5 +3,7 @@ public J{
         System.out.println("Hello World");
         private String name;
         private String address;
+        private String email;
+        private String phone;
     }
 }
