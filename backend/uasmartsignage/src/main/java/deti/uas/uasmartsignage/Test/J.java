8a79af4c7beac94b3607d74d26bea1d168b5265c
DiogoSilva1904 <diogompsilva2000@gmail.com>
2024-04-13 18:01:27 +0100

    Merge with Dev

--- backend/uasmartsignage/src/main/java/deti/uas/uasmartsignage/Test/J.java
+++
@@ -1,9 +0,0 @@
-public J{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        private String name;
-        private String address;
-        private String email;
-        private String phone;
-    }
-}
