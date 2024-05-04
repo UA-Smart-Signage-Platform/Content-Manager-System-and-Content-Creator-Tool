d6660dc5f0d47c997e992435ffc50967c7fa8f21
pedro <pd.pinho@ua.pt>
2024-05-04 16:10:11 +0100

    added info hover

--- frontend/src/components/PageTitle/StartTitleHtml.jsx
+++ frontend/src/components/PageTitle/StartTitleHtml.jsx
@@ -1,7 +1,10 @@
-import { MdAccountCircle, MdInfoOutline, MdMonitor } from "react-icons/md";
-
+import { MdAccountCircle, MdInfoOutline, MdMonitor, MdArrowDropUp } from "react-icons/md";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 function StartTitleHtml( {page} ){
+    const [displayInfo, setDisplayInfo] = useState(false);
+
     if (page === "default")
         return (
         <div className="flex">
@@ -21,18 +24,37 @@ function StartTitleHtml( {page} ){
         )
     if (page === "schedule")
         return(
-            <span className="font-bold text-3xl flex flex-row place-content-end items-end">
+            <div className="font-bold text-3xl flex flex-row place-content-end items-end">
                 Schedule
-                <MdInfoOutline className="ml-2 w-6 cursor-pointer"/>
-            </span>
-    )
+                <motion.span
+                    whileHover={{ scale: 1.2 }}
+                    onMouseEnter={() => setDisplayInfo(true)}
+                    onMouseLeave={() => setDisplayInfo(false)}
+                    className="ml-2 relative"
+                >
+                    <MdInfoOutline className="w-6 cursor-pointer" />
+                    {displayInfo &&
+                        <>
+                            <motion.span
+                                className="absolute translate-x-[-50%] min-w-32 max-w-32 bg-black text-white text-xs rounded py-1 px-3"
+                            >
+                                Tooltip center
+                                
+                            </motion.span>
+                            <MdArrowDropUp className="absolute top-0"/>
+                        </>
+
+                    }
+                </motion.span>
+            </div>
+        )
     if (page === "monitor")
         return(
             <span className="font-bold text-3xl flex flex-row place-content-end items-end">
                 <MdMonitor className="mx-2 text-4xl"/>
                 Monitor
             </span>
-    )
+        )
 }
 
 export default StartTitleHtml
