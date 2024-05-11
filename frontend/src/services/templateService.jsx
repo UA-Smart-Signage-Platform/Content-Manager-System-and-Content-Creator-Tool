fe1e09c97bd48bf996e65cabbb63a0ffe0b2749d
Tomas Victal <tomasvictal@gmail.com>
2024-05-11 13:04:38 +0100

    added new funcs to services

--- frontend/src/services/templateService.jsx
+++ frontend/src/services/templateService.jsx
@@ -3,6 +3,9 @@ import client from "./client";
 const templateservice = {
     async getTemplates() {
         return await client.get("/templates");
+    },
+    async saveTemplate(template){
+        return await client.post("/templates",template)
     }
 }
 
