5f3b2fba35c947db79eb15c39dd0277d4d434545
Tomas Victal <tomasvictal@gmail.com>
2024-05-11 13:03:02 +0100

    added widget modal

---
+++ frontend/src/components/Portals/WidgetsModal.jsx
@@ -0,0 +1,65 @@
+import { createPortal } from 'react-dom';
+import { MdArrowBack } from "react-icons/md";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import DataTable from 'react-data-table-component';
+
+const columns =[
+    {
+        selector: row => row.name
+    }
+]
+const customStyles = {
+    head: {
+        style: {
+            fontSize: '20px',
+        },
+    },
+    rows: {
+        style: {
+            fontSize: '13px',
+        },
+    }
+};
+
+function WidgetsModal( { Widgets, WidgetsList, setWidgetList,setShowPortal } ) {
+
+    
+    return (
+    <>
+        {createPortal(
+            <div className="fixed z-10 top-0 h-screen w-screen backdrop-blur-sm flex">
+                <div className="bg-black h-screen w-screen opacity-75"></div>
+                <div className="absolute text-gray-50 h-screen w-screen flex items-center">
+                    <div className="bg-[#fafdf7] text-[#101604] h-[70%] w-[50%] mx-auto rounded-xl p-[2%]">
+                        <div className="h-[10%] flex">
+                            <button onClick={() => setShowPortal(false)} className="flex flex-row">
+                                <MdArrowBack className="w-7 h-7 mr-2"/> 
+                                <span className="text-xl">Go back</span>
+                            </button>
+                        </div>
+                        <div className="h-[90%] p-[4%]">
+                            <DataTable
+                                data={Widgets}
+                                columns={columns}
+                                customStyles={customStyles}
+                                pagination
+                                theme="solarized"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>,
+        document.body
+        )}
+    </>
+);
+}
+
+WidgetsModal.propTypes = {
+    Widgets: PropTypes.array.isRequired,
+    setWidgetList: PropTypes.func.isRequired,
+    setShowPortal: PropTypes.func.isRequired,
+}
+
+export default WidgetsModal;
--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -8,4 +8,5 @@ export {default as MovableDiv} from "./MovableDiv"
 export {default as MediaFileModal} from './Portals/MediaFileModal';
 export {default as MediaFolderModal} from './Portals/MediaFolderModal';
 export {default as PendingMonitorsModal} from './Portals/PendingMonitorsModal';
+export {default as WidgetsModal} from "./Portals/WidgetsModal";
 export {default as ScheduleModal} from './Portals/ScheduleModal';
