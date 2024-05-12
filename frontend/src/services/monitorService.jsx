49339e60d8070036e011b1a521f0d297d017b20a
Tomas Victal <tomasvictal@gmail.com>
2024-05-12 11:44:52 +0100

    fix

--- frontend/src/services/monitorService.jsx
+++ frontend/src/services/monitorService.jsx
@@ -8,6 +8,10 @@ const monitorService = {
     async getMonitorById(id){
         return await client.get(`/monitors/${id}`)
     },
+    
+    async acceptMonitor() {
+        return await client.post("/monitors")
+    },
     async getMonitorsByGroup(id){
         return await client.get(`/monitors/group/${id}`)
     },
