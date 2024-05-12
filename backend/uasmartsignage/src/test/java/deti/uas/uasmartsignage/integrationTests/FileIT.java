8094810799ee389edbd8a2aa0d919319f8dbe642
DiogoSilva1904 <diogompsilva2000@gmail.com>
2024-05-12 18:40:23 +0100

    filesIT update

--- backend/uasmartsignage/src/test/java/deti/uas/uasmartsignage/integrationTests/FileIT.java
+++ backend/uasmartsignage/src/test/java/deti/uas/uasmartsignage/integrationTests/FileIT.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.io.ByteArrayResource;
@@ -31,8 +30,8 @@ import java.util.Objects;
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.profiles.active=integration-test"})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@AutoConfigureMockMvc(addFilters = false)
-class FileServiceIT{
+@Order(1)
+class FileIT {
 
     @Container
     public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
@@ -58,19 +57,15 @@ class FileServiceIT{
 
     @BeforeAll
     static void setup(@LocalServerPort int port1) {
-        // Prepare the request body with valid credentials
         String username = "admin";
         String password = "admin";
         String requestBody = "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}";
 
-        // Prepare the request headers
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
 
-        // Create the request entity
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
 
-        // Make a POST request to your authentication endpoint to get the JWT token
         ResponseEntity<String> response = restTemplate1.exchange(
                 "http://localhost:"+ port1 + "/api/login",
                 HttpMethod.POST,
@@ -78,10 +73,8 @@ class FileServiceIT{
                 String.class
         );
 
-        // Ensure that the request was successful (HTTP status code 200)
         assertEquals(200, response.getStatusCodeValue());
 
-        // Extract the JWT token from the response body
         String responseBody = response.getBody();
 
         JsonElement jsonElement = JsonParser.parseString(responseBody);
