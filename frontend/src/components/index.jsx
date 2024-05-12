925ae127f8f92d841cb77849bb724d172464fca8
pedro <pd.pinho@ua.pt>
2024-05-12 14:13:18 +0100

    added functionality to safely delete rule

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -10,3 +10,4 @@ export {default as MediaFileModal} from './Portals/MediaFileModal';
 export {default as MediaFolderModal} from './Portals/MediaFolderModal';
 export {default as PendingMonitorsModal} from './Portals/PendingMonitorsModal';
 export {default as ScheduleModal} from './Portals/ScheduleModal';
+export {default as ScheduleDeleteModal} from './Portals/ScheduleDeleteModal';
