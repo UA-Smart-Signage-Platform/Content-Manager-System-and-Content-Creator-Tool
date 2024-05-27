2418fed60262d5a2b4e765062fbf1749e139ccd1
miguel <miguelcruzeiro70@gmail.com>
2024-05-27 23:42:11 +0100

    sonar fixes

--- backend/uasmartsignage/src/main/java/deti/uas/uasmartsignage/Controllers/TemplateController.java
+++ backend/uasmartsignage/src/main/java/deti/uas/uasmartsignage/Controllers/TemplateController.java
@@ -2,8 +2,6 @@ package deti.uas.uasmartsignage.Controllers;
 
 import deti.uas.uasmartsignage.Models.Template;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
