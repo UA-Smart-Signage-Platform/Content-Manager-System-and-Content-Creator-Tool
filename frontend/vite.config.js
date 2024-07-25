ced135df12d2baf278d925a0ac413ac4b126546a
pedro <pd.pinho@ua.pt>
2024-07-25 11:06:07 +0100

    fixed blank page error by adding configs to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
     host: true,
     port: 8000, // This is the port which we will use in docker
@@ -12,5 +14,10 @@ export default defineConfig({
      watch: {
        usePolling: true
      }
-  }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 })
