13c2a21ef45851d118c8b16561c06271de346507
pedro <pd.pinho@ua.pt>
2024-07-31 17:52:28 +0100

    added fake sign in button

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,6 +1,6 @@
 import { MdOutlineDashboard,MdOutlinePermMedia,
     MdCalendarToday,MdMonitor,
-    MdOutlineBrandingWatermark,MdOutlineWbSunny, MdOutlineSupervisorAccount, MdOutlineLogout } from "react-icons/md";
+    MdOutlineBrandingWatermark,MdOutlineWbSunny, MdOutlineSupervisorAccount, MdOutlineLogout, MdOutlineLogin } from "react-icons/md";
 import { LuMoon } from "react-icons/lu";
 import  Logo  from '../../static/logo.svg?react'
 import { AnimatePresence, motion } from "framer-motion";
@@ -37,36 +37,41 @@ function NavBar({setLogged}) {
             <div className="w-full h-[16%]">
                 <Logo className="w-full h-full pl-2 pr-1" />
             </div>
-            <Link to={"dashboard"} className="w-full h-[5%]">
-                <MdOutlineDashboard className=" h-full w-full px-3.5 text-white"/>
-            </Link>
-            <Link to={"media/home"} className="w-full h-[5%]">
-                <MdOutlinePermMedia className=" h-full w-full px-3.5"/>
-            </Link>
-            <Link to={"schedule"} className="w-full h-[5%]">
-                <MdCalendarToday className=" h-full w-full px-3.5"/>
-            </Link>
-            <Link to={"monitors"} className="w-full h-[5%]">
-                <MdMonitor className=" h-full w-full px-3.5"/>
-            </Link>
-            {userRole === "ROLE_ADMIN" && (
-                <Link to={"contentcreator"} className="w-full h-[5%]">
-                    <MdOutlineBrandingWatermark className=" h-full w-full px-3.5"/>
-                </Link>
-            )}
-            {userRole === "ROLE_ADMIN" && (
-                <Link to={"admin"} className="w-full h-[5%]">
-                    <MdOutlineSupervisorAccount className=" h-full w-full px-3.5"/>
-                </Link>
-            )}
-            {isLoggedIn && (
-                <button className="w-full h-[5%] text-lg justify-evenly cursor-pointer" onClick={handleLogout}>
-                    <MdOutlineLogout className=" h-full w-full px-3.5"/>
-                </button>
-            )}
-            
-            
+            {!isLoggedIn ? 
+                (<button className="w-full h-[5%] text-lg justify-evenly cursor-pointer">
+                    <MdOutlineLogin className=" h-full w-full px-3.5"/>
+                </button>) 
+                : 
+                (<>
+                    <Link to={"dashboard"} className="w-full h-[5%]">
+                        <MdOutlineDashboard className=" h-full w-full px-3.5 text-white"/>
+                    </Link>
+                    <Link to={"media/home"} className="w-full h-[5%]">
+                        <MdOutlinePermMedia className=" h-full w-full px-3.5"/>
+                    </Link>
+                    <Link to={"schedule"} className="w-full h-[5%]">
+                        <MdCalendarToday className=" h-full w-full px-3.5"/>
+                    </Link>
+                    <Link to={"monitors"} className="w-full h-[5%]">
+                        <MdMonitor className=" h-full w-full px-3.5"/>
+                    </Link>
+                    {userRole === "ROLE_ADMIN" && (
+                        <Link to={"contentcreator"} className="w-full h-[5%]">
+                            <MdOutlineBrandingWatermark className=" h-full w-full px-3.5"/>
+                        </Link>
+                    )}
+                    {userRole === "ROLE_ADMIN" && (
+                        <Link to={"admin"} className="w-full h-[5%]">
+                            <MdOutlineSupervisorAccount className=" h-full w-full px-3.5"/>
+                        </Link>
+                    )}
+                    <button className="w-full h-[5%] text-lg justify-evenly cursor-pointer" onClick={handleLogout}>
+                        <MdOutlineLogout className=" h-full w-full px-3.5"/>
+                    </button>
+                </>
+            )} 
         </div>
+        
         <AnimatePresence>
             {isShow && <motion.div className=" flex flex-col w-[10%] bg-black h-[100vh] gap-10 z-0 text-white fixed left-[65px] font-Lexend text-2xl font-semibold"
                 initial={{x:-200}}
@@ -80,32 +85,38 @@ function NavBar({setLogged}) {
                     <h1>UA SMART</h1>
                     <h1 className="ml-8">SIGNAGE</h1> 
                 </div>
-                <Link to={"dashboard"} className="w-full h-[5%] flex items-center">
-                    Dashboard
-                </Link>
-                <Link to={"media/home"} className="w-full h-[5%] flex items-center">
-                    Media
-                </Link>
-                <Link to={"schedule"} className="w-full h-[5%] flex items-center">
-                    Schedule
-                </Link>
-                <Link to={"monitors"} className="w-full h-[5%] flex items-center">
-                    Monitors
-                </Link>
-                {userRole === "ROLE_ADMIN" && (
-                    <Link to={"contentcreator"} className="w-full h-[5%] flex items-center">
-                        Content Creator Tool
+                {!isLoggedIn ? 
+                (<button className="w-full h-[5%] flex items-center">
+                    Sign in
+                </button>) 
+                : 
+                (<>
+                    <Link to={"dashboard"} className="w-full h-[5%] flex items-center">
+                        Dashboard
                     </Link>
-                )}
-                {userRole === "ROLE_ADMIN" && (
-                    <Link to={"admin"} className="w-full h-[5%] flex items-center">
-                        Users
+                    <Link to={"media/home"} className="w-full h-[5%] flex items-center">
+                        Media
                     </Link>
-                )}
-                {isLoggedIn && ( 
+                    <Link to={"schedule"} className="w-full h-[5%] flex items-center">
+                        Schedule
+                    </Link>
+                    <Link to={"monitors"} className="w-full h-[5%] flex items-center">
+                        Monitors
+                    </Link>
+                    {userRole === "ROLE_ADMIN" && (
+                        <Link to={"contentcreator"} className="w-full h-[5%] flex items-center">
+                            Content Creator Tool
+                        </Link>
+                    )}
+                    {userRole === "ROLE_ADMIN" && (
+                        <Link to={"admin"} className="w-full h-[5%] flex items-center">
+                            Users
+                        </Link>
+                    )}
                     <button className="w-full h-[5%] flex items-center" onClick={handleLogout}>
-                        Logout
+                        Sign out
                     </button>
+                </>
                 )}
 
                 <button className="w-full h-[5%] flex items-center mt-auto text-lg justify-evenly cursor-pointer" onClick={() => changeTheme(theme === "dark" ? "light" : "dark")}>
