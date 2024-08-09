f20a638ad958f958bf04480848d2b7a70b9a65a1
fungame2270 <tomasvictal@gmail.com>
2024-08-09 17:23:29 +0100

    added config for particles

--- frontend/src/routes/Wso2Login/index.jsx
+++ frontend/src/routes/Wso2Login/index.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
 import loginService from '../../services/loginService';
 import { useNavigate } from "react-router-dom";
+import  Logo  from '../../static/green-name.svg?react';
+import { greenParticles,nyanParticles } from './particlesConfigs';
 
 const CLIENT_ID = import.meta.env.REACT_APP_WSO2_CLIENT_ID;
 const REDIRECT_URI = import.meta.env.REACT_APP_WSO2_REDIRECT_URI;
@@ -23,8 +27,23 @@ const setWithExpiry = (key, value, ttl) => {
 const Wso2Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loginRoot,setLoginRoot] = useState(false);
+  const [nyan,setNyan] = useState(0);
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
   useEffect(() => {
     localStorage.removeItem('access_token');
@@ -53,51 +72,55 @@ const Wso2Login = () => {
     }
   };
 
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', padding: '20px' }}>
-      <div style={{ width: '45%', marginBottom: '20px' }}>
-        <div style={{ padding: '20px', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
-          <h2>Login</h2>
-          <div style={{ marginBottom: '15px' }}>
-            <label htmlFor="username" style={{ display: 'block', marginBottom: '5px' }}>Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              style={{ width: '100%', padding: '10px', border: '1px solid #ccc', borderRadius: '4px' }}
-              required
-            />
-          </div>
-          <div style={{ marginBottom: '15px' }}>
-            <label htmlFor="password" style={{ display: 'block', marginBottom: '5px' }}>Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              style={{ width: '100%', padding: '10px', border: '1px solid #ccc', borderRadius: '4px' }}
-              required
-            />
-          </div>
-          {error && (
-            <div style={{ color: 'red', marginBottom: '15px' }}>
-              {error}
-            </div>
-          )}
-
-          <button onClick={handleLogin} style={{ backgroundColor: '#4caf50', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Login</button>
-        </div>
-      </div>
-
-      <div style={{ width: '45%' }}>
-        <div style={{ padding: '20px', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
-          <h2>UA Login</h2>
-          <button onClick={redirectToLogin} style={{ backgroundColor: '#5a67d8', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer', marginTop: '10px' }}>UA Login</button>
-        </div>
-      </div>
-    </div>
+  const options = useMemo(
+    ()=> {
+      if(nyan > 9){
+        return nyanParticles
+      }else{
+        return greenParticles
+      }
+    },
+    [nyan],
   );
+  if(init){
+    return (
+      <div className='h-full w-full relative'>
+        <Particles
+          id="tsparticles"
+          className='h-full w-full absolute'
+          style={{zIndex:-1}}
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+        <div className='flex flex-col gap-20 w-[50%] mx-auto items-center z-40 pt-[10%] relative'>
+          <div onClick={()=>setNyan(nyan+1)}><Logo></Logo></div>
+          {loginRoot ? 
+          <div className='flex flex-col gap-6 items-center justify-center'>
+            <div className='w-full'>
+              <p className=' justify-start w-full text-2xl text-[#95A967]'>Sign in as Root</p>
+              <input className=' border-secondary border-2 rounded-md font-bold p-1 text-xl bg-secondaryLight'
+                    placeholder='password'
+              />
+            </div>
+            <button className='bg-primary font-bold text-lg p-1 rounded-md w-full'>Sign In</button>
+            <button className='bg-secondary font-bold text-lg p-1 rounded-md w-[75%]'
+                    onMouseDown={()=> setLoginRoot(!loginRoot)}>
+                    Back
+            </button>
+          </div>
+          : 
+          <div className='flex flex-col gap-6 items-center justify-center'>
+            <button className='bg-primary font-bold text-xl p-3 rounded-md px-16'>Sign in with IDP</button>
+            <button className='bg-[#95A967] font-bold text-lg p-1 rounded-md w-[75%]'
+                    onMouseDown={()=> setLoginRoot(!loginRoot)}>
+                    Admin Sign in
+            </button>
+          </div>
+          }
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Wso2Login;
