634105053f5ac806af0cec86e461a97c66b78896
fungame2270 <tomasvictal@gmail.com>
2024-08-13 13:56:00 +0100

    refactor somo styling of code

--- frontend/src/routes/Wso2Login/SignInDiv.jsx
+++ frontend/src/routes/Wso2Login/SignInDiv.jsx
@@ -17,32 +17,33 @@ function SignInDiv({setNyan}) {
 
     },[clickTimes])
 
+    const loginLayout = loginRoot ? 
+      <div className='flex flex-col gap-6 items-center justify-center'>
+        <div className='w-full'>
+          <p className=' justify-start w-full text-2xl text-[#95A967]'>Sign in as Root</p>
+          <input className=' border-secondary border-2 rounded-md font-bold p-1 text-xl bg-secondaryLight'
+                placeholder='password'
+          />
+        </div>
+        <button className='bg-primary font-bold text-lg p-1 rounded-md w-full'>Sign In</button>
+        <button className='bg-secondary font-bold text-lg p-1 rounded-md w-[75%]'
+                onMouseDown={()=> setLoginRoot(!loginRoot)}>
+                Back
+        </button>
+      </div>
+      : 
+      <div className='flex flex-col gap-6 items-center justify-center'>
+        <button className='bg-primary font-bold text-xl p-3 rounded-md px-16'>Sign in with IDP</button>
+        <button className='bg-[#95A967] font-bold text-lg p-1 rounded-md w-[75%]'
+                onMouseDown={()=> setLoginRoot(!loginRoot)}>
+                Admin Sign in
+        </button>
+      </div>
+
     return(
         <div className='flex flex-col gap-20 w-[50%] mx-auto items-center z-40 pt-[10%] relative'>
           <div onClick={()=>clickTimeCheck()}><Logo></Logo></div>
-          {loginRoot ? 
-          <div className='flex flex-col gap-6 items-center justify-center'>
-            <div className='w-full'>
-              <p className=' justify-start w-full text-2xl text-[#95A967]'>Sign in as Root</p>
-              <input className=' border-secondary border-2 rounded-md font-bold p-1 text-xl bg-secondaryLight'
-                    placeholder='password'
-              />
-            </div>
-            <button className='bg-primary font-bold text-lg p-1 rounded-md w-full'>Sign In</button>
-            <button className='bg-secondary font-bold text-lg p-1 rounded-md w-[75%]'
-                    onMouseDown={()=> setLoginRoot(!loginRoot)}>
-                    Back
-            </button>
-          </div>
-          : 
-          <div className='flex flex-col gap-6 items-center justify-center'>
-            <button className='bg-primary font-bold text-xl p-3 rounded-md px-16'>Sign in with IDP</button>
-            <button className='bg-[#95A967] font-bold text-lg p-1 rounded-md w-[75%]'
-                    onMouseDown={()=> setLoginRoot(!loginRoot)}>
-                    Admin Sign in
-            </button>
-          </div>
-          }
+          {loginLayout}
         </div>
     )
 }
