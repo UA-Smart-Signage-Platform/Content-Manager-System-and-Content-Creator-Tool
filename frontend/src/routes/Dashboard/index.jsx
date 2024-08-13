825a51001367ee5e2753c25744201ba7ee5dfb2d
pdPinho <pd.pinho@ua.pt>
2024-08-13 13:54:19 +0100

    added monitor list to dashboard

--- frontend/src/routes/Dashboard/index.jsx
+++ frontend/src/routes/Dashboard/index.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import { useQueries, useQueryClient } from '@tanstack/react-query';
 import { DashboardGraph, GroupBar, PageTitle } from '../../components'
-import { MdBugReport, MdWarning } from 'react-icons/md';
+import { MdArrowBack, MdBugReport, MdRemoveRedEye, MdWarning } from 'react-icons/md';
 import monitorService from '../../services/monitorService';
+import DataTable from 'react-data-table-component';
 
 
 
 function Dashboard() {
     const queryClient = useQueryClient();
     const [groupId, setGroupId] = useState(null);
+    const [showMonitorList, setShowMonitorList] = useState(false);
+    const [selectedOnline, setSelectedOnline] = useState(false);
 
     const [onlineMonitorsQuery, offlineMonitorsQuery, onlineMonitorsByGroupQuery, offlineMonitorsByGroupQuery] = useQueries({
         queries : [
@@ -16,22 +19,22 @@ function Dashboard() {
                 queryKey: ['onlineMonitors', { onlineStatus: true }],
                 queryFn: () => monitorService.getMonitors(true),
                 enabled: groupId === null
-              },
-              {
+            },
+            {
                 queryKey: ['offlineMonitors', { onlineStatus: false }],
                 queryFn: () => monitorService.getMonitors(false),
                 enabled: groupId === null
-              },
-              {
+            },
+            {
                 queryKey: ['onlineMonitorsByGroup', { groupId, onlineStatus: true }],
                 queryFn: () => monitorService.getMonitorsByGroup(groupId, true),
                 enabled: groupId != null
-              },
-              {
+            },
+            {
                 queryKey: ['offlineMonitorsByGroup', { groupId, onlineStatus: false }],
                 queryFn: () => monitorService.getMonitorsByGroup(groupId, false),
                 enabled: groupId != null
-              }
+            }
         ]
     })
 
@@ -47,10 +50,10 @@ function Dashboard() {
         </div>
     );
 
-    const DataDisplay = ({ numberMonitors }) => (
+    const DataDisplay = ({ numberMonitors, message }) => (
         <div className="flex flex-col items-center place-content-center">
           <span className="text-6xl pb-2 font-medium">{numberMonitors}</span>
-          <span className="text-lg">Online</span>
+          <span className="text-lg">{message}</span>
         </div>
     );
 
@@ -65,7 +68,7 @@ function Dashboard() {
             return <ErrorDisplay message={query.error.message} />;
         }
 
-        return <DataDisplay numberMonitors={query.data.data.length} />;
+        return <DataDisplay numberMonitors={query.data.data.length} message={"Online"} />;
     }
 
     const showOfflineMonitors = () => {
@@ -79,8 +82,167 @@ function Dashboard() {
             return <ErrorDisplay message={query.error.message} />;
         }
 
-        return <DataDisplay numberMonitors={query.data.data.length} />;
+        return <DataDisplay numberMonitors={query.data.data.length} message={"Offline"} />;
     }
+
+
+    const showAllStatusOrSelectedStatus = () => {
+        if (showMonitorList) {
+            return (
+                <div className="w-[25%] h-full">
+                    <button onClick={() => {setShowMonitorList(false); setSelectedOnline(false)}} className="flex flex-row h-[10%] text-3xl font-medium items-center pt-2 pl-2">
+                        <MdArrowBack className="w-7 h-7 mr-2"/> 
+                        <span className="text-xl">Go back</span>
+                    </button>
+                    <div className="h-[90%] flex flex-row items-center place-content-center">
+                        <div className={`hover:opacity-90 h-[55%] w-[45%] rounded-md flex flex-col items-center place-content-center bg-gradient-to-b ${selectedOnline ? "from-[#96D600] from-25% to-[#76A701]" : "from-[#D12E2E] from-46% to-[#A12626] to-90%"}`}>
+                            {selectedOnline ? showOnlineMonitors() : showOfflineMonitors()}
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className="w-[50%] h-full">
+                    <div className="h-[10%] text-3xl font-medium items-center flex pt-2 pl-2">
+                        Monitors
+                    </div>
+                    <div className="h-[90%] flex flex-row items-center place-content-center">
+                        <button onClick={()=>{setShowMonitorList(true); setSelectedOnline(true)}} className="hover:opacity-90 cursor-pointer h-[55%] w-[22%] rounded-md bg-gradient-to-b from-[#96D600] from-25% to-[#76A701] flex flex-col items-center place-content-center mr-[10%]">
+                            {showOnlineMonitors()}
+                        </button>
+                        <button onClick={()=>setShowMonitorList(true)} className="hover:opacity-90 cursor-pointer h-[55%] w-[22%] rounded-md bg-gradient-to-b from-[#D12E2E] from-46% to-[#A12626] to-90% flex flex-col items-center place-content-center mr-[10%]">
+                            {showOfflineMonitors()}
+                        </button>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    const customStyles = {
+        head: {
+            style: {
+                fontWeight: 400,
+            },
+        },
+        headRow: {
+            style: {
+                fontSize: '16px',
+                minHeight: '32px',
+                backgroundColor: "#C1C1C1",
+                borderradius: '20px',
+                borderBottomWidth: '0px'
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '16px',
+            },
+        },
+        rows: {
+            style: {
+                fontSize: '16px',
+                minHeight: '32px',
+                backgroundColor: "#E9E9E9",
+                '&:not(:last-of-type)': {
+                    borderBottomStyle: 'solid',
+                    borderBottomWidth: '1px',
+                    borderBottomColor: "#A7A8AA",
+                },
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '10px',
+                paddingRight: '0px',
+                wordBreak: 'break-word',
+            }
+        }
+    };
+
+    const paginationComponentOptions = {
+        noRowsPerPage: true,
+        selectAllRowsItem: false,
+    };
+
+    const columns = [
+        {
+            name: (                
+                <div className="flex flex-row">
+                     Name
+                </div>
+            ),
+            selector: row => <div className='flex items-center'>
+                                <MdRemoveRedEye className="border h-5 w-5 rounded border-black mr-3" /> 
+                                <span>{row.name}</span> 
+                                <span className=" ml-7">/</span>
+                            </div>,
+            sortable: true,
+        },
+        {
+            name: (                
+                <div className="flex flex-row">
+                    Group
+                </div>
+            ),
+            selector: row => !row.group.madeForMonitor ? row.group.name : "-----",
+            sortable: true,
+        },
+        {
+            name: (                
+                <div className="flex flex-row">
+                     Warns (5 days)
+                </div>
+            ),
+            selector: row => row.warnings
+        },
+        {
+            name: (                
+                <div className="flex flex-row">
+                     Online for
+                </div>
+            ),
+            selector: row => row.online
+        }   
+    ];
+
+    const showGraphOrMonitorsList = () => {
+        if (showMonitorList){
+            return (
+                <div className="w-[75%] h-full pr-6 pt-4 pb-4 rounded">
+                    <DataTable
+                    pointerOnHover
+                    highlightOnHover
+                    pagination
+                    paginationComponentOptions={paginationComponentOptions}
+                    onRowClicked={(row) => navigate('/monitor/' + row.id, { state: row })}
+                    columns={columns}
+                    data={(groupId === null ? 
+                            (selectedOnline ? onlineMonitorsQuery.data.data : offlineMonitorsQuery.data.data) 
+                            : 
+                            (selectedOnline ? onlineMonitorsByGroupQuery.data.data : offlineMonitorsByGroupQuery.data.data))}
+                    theme="solarized"
+                    customStyles={customStyles}/>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className="w-[50%] h-full flex flex-col place-content-center">
+                    <DashboardGraph 
+                    data={data1} 
+                    xLabel={"hour"} 
+                    yLabel={"monitors"}
+                    height={"75%"}
+                    title={<><MdWarning className="w-6 h-6 mx-1"/> Downtime (today)</>}
+                    linkTo={"/"} />
+                </div>
+            )
+        }
+    }
+
 
 
     const data1 = [];
@@ -117,28 +279,8 @@ function Dashboard() {
                     <div id="content" className="flex flex-col w-full p-3">
                         <div className="h-[49%]">
                             <div className="h-full pt-3 pr-2 pl-2 pb-3 flex flex-row">
-                                <div className="w-[50%] h-full">
-                                    <div className="h-[10%] text-3xl font-medium items-center flex pt-2 pl-2">
-                                        Monitors
-                                    </div>
-                                    <div className="h-[90%] flex flex-row items-center place-content-center">
-                                        <div className="hover:opacity-90 cursor-pointer h-[55%] w-[22%] rounded-md bg-gradient-to-b from-[#96D600] from-25% to-[#76A701] flex flex-col items-center place-content-center mr-[10%]">
-                                            {showOnlineMonitors()}
-                                        </div>
-                                        <div className="hover:opacity-90 cursor-pointer h-[55%] w-[22%] rounded-md bg-gradient-to-b from-[#D12E2E] from-46% to-[#A12626] to-90% flex flex-col items-center place-content-center mr-[10%]">
-                                            {showOfflineMonitors()}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="w-[50%] h-full flex flex-col place-content-center">
-                                    <DashboardGraph 
-                                        data={data1} 
-                                        xLabel={"hour"} 
-                                        yLabel={"monitors"}
-                                        height={"75%"}
-                                        title={<><MdWarning className="w-6 h-6 mx-1"/> Downtime (today)</>}
-                                        linkTo={"/"} />
-                                </div>
+                                {showAllStatusOrSelectedStatus()}
+                                {showGraphOrMonitorsList()}
                             </div>
                         </div>
                         <div id="dividerHr" className="border-[1px] border-secondary flex-col"/>
