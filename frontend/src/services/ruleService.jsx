1c5a96b7bcb92c2ac5bd35c7da0b8b6a06696bfd
pedro <pd.pinho@ua.pt>
2024-09-12 16:59:03 +0100

    updated schedule to match backend

--- frontend/src/services/ruleService.jsx
+++ frontend/src/services/ruleService.jsx
@@ -3,6 +3,10 @@ import client from "./client";
 const ruleService = {
     async getRulesByGroupId(id) {
         return await client.get(`/rules/group/${id}`, id);
+    },
+
+    async addRule(rule) {
+        return await client.post("/rules", rule);
     }
 }
 
